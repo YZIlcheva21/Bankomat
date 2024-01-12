4b1e6148bb4ace3e42bdb10f88cef28dae78d873
YZIlcheva21 <YZIlcheva21@codingburgas.bg>
2024-01-12 20:50:54 +0200

    Add comments

--- Source/main/main/mainMenu.h
+++ Source/main/main/mainMenu.h
@@ -2,6 +2,8 @@
 #include <string>
 using namespace std;
 
+//Displays the menu options with highlighting for the selected option.
 void printMenuOptions(string menuOptions[], int selectedOption);
 
+//Displays the main menu and handles user input to navigate through options.
 void mainMenu(string menuOptions[]);
